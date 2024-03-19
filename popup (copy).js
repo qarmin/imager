34119136c0dee1084767a8226739a7365c319a41
Rafał Mikrut <mikrutrafal@protonmail.com>
2024-03-19 18:09:50 +0100

    F

--- popup (copy).js
+++ popup (copy).js
@@ -115,3 +115,10 @@ function observeDocument() {
 
 // Uruchomienie monitorowania zmian w obiekcie document
 observeDocument();
+
+function openPage() {
+  browser.tabs.create({
+    url: "https://developer.mozilla.org",
+  });
+}
+browser.browserAction.onClicked.addListener(openPage);
